c7ac9bb99e36adaa2eda9fcdc3d8dee7ec0c3028
auriga11 <112501413+auriga11@users.noreply.github.com>
2025-02-04 13:17:56 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,6 +1,7 @@
 from pyrogram import __version__
 from bot import Bot
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.enums import Parsemode
 
 from config import PRICE1, PRICE2, PRICE3, PRICE4, PRICE5, UPI_ID, UPI_IMAGE_URL, SCREENSHOT_URL
 
@@ -27,8 +28,8 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             pass
     elif data == "buy_prem":
         await query.message.edit_text(
-            text=f"👋 {query.from_user.username}\n\n🎖️ **Available Membership Plans:**\n\n🔹 **{PRICE1}** – 7 Days Prime Membership\n🔹 **{PRICE2}** – 1 Month Prime Membership\n🔹 **{PRICE3}** – 3 Months Prime Membership\n🔹 **{PRICE4}** – 6 Months Prime Membership\n🔹 **{PRICE5}** – 1 Year Prime Membership\n\n🔥 **Exclusive Categories:**\n🔸 **Desi MMS Membership** – {PRICE1}\n🔸 **Spy Membership** *(B@th + Pi$$ + Hidden Cam)* – {PRICE2}\n🔸 **Sn@p Le@ks Membership** – {PRICE3}\n🔸 **Models Membership** *(Ind + Pak + International)* – {PRICE4}\n🔸 **Mega Links Membership** – {PRICE5}\n\n💰 **Payment Details:**\n💵 **UPI ID:** <code>{UPI_ID}</code>\n📸 **QR Code:** [Click Here to Scan]({UPI_IMAGE_URL})\n♻️ **For Crypto Payments:** [@Crypto_Paymentss](https://t.me/Crypto_Paymentss)\n\n✅ **Perks of Membership:**\n✔️ Lifetime Access\n✔️ Fresh Content Uploaded Every Weekend\n\n‼️ *Must send a screenshot after payment for verification!*",
-            disable_web_page_preview=True,
+            text=f"👋 {query.from_user.username}\n\n🎖️ **Available Bot Plans:**\n\n🔹 **₹40** – 7 Days Bot Membership\n🔹 **₹119** – 1 Month Bot Membership\n🔹 **₹279** – 3 Months Bot Membership\n🔹 **₹529** – 6 Months Prime Membership\n🔹 **₹749** – 1 Year Bot Membership\n\n🔥 **Exclusive Categories:**\n🔸 **Desi MMS Membership** – {PRICE1}\n🔸 **Spy Membership** *(B@th + Pi$$ + Hidden Cam)* – {PRICE2}\n🔸 **Sn@p Le@ks Membership** – {PRICE3}\n🔸 **Models Membership** *(Ind + Pak + International)* – {PRICE4}\n🔸 **Mega Links Membership** – {PRICE5}\n\n💰 **Payment Details:**\n💵 **UPI ID:** <code>{UPI_ID}</code>\n📸 **QR Code:** [Click Here to Scan]({UPI_IMAGE_URL})\n♻️ **For Crypto Payments:** [@Crypto_Paymentss](https://t.me/Crypto_Paymentss)\n\n✅ **Perks of Membership:**\n✔️ Lifetime Access\n✔️ Fresh Content Uploaded Every Weekend\n\n‼️ *Must send a screenshot after payment for verification!*",
+            disable_web_page_preview=True,parse_mode=ParseMode.MARKDOWN,
             reply_markup = InlineKeyboardMarkup(
                 [   
                     [
